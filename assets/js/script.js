d41242b204cbfa1cbac1d9c08834f1553a4f47c4
Diwas Poudel <n_diwas.poudel@cashdirector.com>
2022-06-07 17:13:16 +0545

    ne changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-$(document).ready(function(){
-    //navigation 
-    $('.navbar-open-btn').click( () => $('.navigation-sidebar').addClass('show-navigation-sidebar'));
-    $('.navbar-close-btn').click(() => $('.navigation-sidebar').removeClass('show-navigation-sidebar'));
+$(document).ready(function () {
+  //navigation
+  $('.navbar-open-btn').click(() =>
+    $('.navigation-sidebar').addClass('show-navigation-sidebar')
+  );
+  $('.navbar-close-btn').click(() =>
+    $('.navigation-sidebar').removeClass('show-navigation-sidebar')
+  );
 
-    // sweet alert
-    $('#spin-btn').click(function(){
-        Swal.fire({
-            icon: 'success',
-            title: '\'Successfully spinned You have earned R26!\'',
-            showConfirmButton: false
-        })
-    })
+  // sweet alert
+  $('#spin-btn').click(function () {
+    Swal.fire({
+      icon: 'success',
+      title: "'R7'",
+      showConfirmButton: false,
+    });
+  });
 });
-
-
-
-
-  
